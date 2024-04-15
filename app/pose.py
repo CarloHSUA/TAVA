f2ff952b20a3c526f36fa61daba435d7bdd2d1f9
Bernabe19 <bernasanchezsos4@gmail.com>
2024-04-15 15:02:22 +0200

    update pose

--- app/pose.py
+++ app/pose.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LogisticRegression
 import joblib
 import pandas as pd
 import xgboost as xgb
+import sys
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
@@ -40,46 +41,110 @@ feature_names = ['x_nose', 'y_nose', 'z_nose', 'x_left_eye_inner', 'y_left_eye_i
        'y_left_foot_index', 'z_left_foot_index', 'x_right_foot_index',
        'y_right_foot_index', 'z_right_foot_index']
 
-modelo_cargado = joblib.load('xgb.pkl')
+modelo_cargado = joblib.load('log_reg.pkl')
+
+mode = sys.argv[1]
+
+if mode == "image":
+      # For static images:
+  IMAGE_FILES = ["tio_flexiones.jpg"]
+  BG_COLOR = (192, 192, 192) # gray
+  with mp_pose.Pose(
+      static_image_mode=True,
+      model_complexity=2,
+      enable_segmentation=True,
+      min_detection_confidence=0.5) as pose:
+    for idx, file in enumerate(IMAGE_FILES):
+      image = cv2.imread(file)
+      image_height, image_width, _ = image.shape
+      # Convert the BGR image to RGB before processing.
+      results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+      inputs = list()
+      try:
+          for i in range(len(results.pose_landmarks.landmark)):
+              inputs.append(results.pose_landmarks.landmark[i].x)
+              inputs.append(results.pose_landmarks.landmark[i].y)
+              inputs.append(results.pose_landmarks.landmark[i].z)
+      except:
+          pass
+      
+      output = modelo_cargado.predict(pd.DataFrame([inputs], columns=feature_names))
+
+      if not results.pose_landmarks:
+        continue
+      print(
+          f'Nose coordinates: ('
+          f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+          f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+      )
+
+      annotated_image = image.copy()
+      # Draw segmentation on the image.
+      # To improve segmentation around boundaries, consider applying a joint
+      # bilateral filter to "results.segmentation_mask" with "image".
+      condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+      # bg_image = np.zeros(image.shape, dtype=np.uint8)
+      # bg_image[:] = BG_COLOR
+      annotated_image = np.where(condition, annotated_image, image)#, bg_image)
+      # Draw pose landmarks on the image.
+      mp_drawing.draw_landmarks(
+          annotated_image,
+          results.pose_landmarks,
+          mp_pose.POSE_CONNECTIONS,
+          landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+      cv2.putText(annotated_image, "Predicted: " + str(activities[output[0]]), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+      cv2.imwrite('annotated_image.png', annotated_image)
+      cv2.imshow('MediaPipe Pose', annotated_image)
+      # Plot pose world landmarks.
+      mp_drawing.plot_landmarks(
+          results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+
+elif mode == "video":
 # For webcam input:
-cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+  cap = cv2.VideoCapture(0)
+  with mp_pose.Pose(
+      min_detection_confidence=0.5,
+      min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+      success, image = cap.read()
+      if not success:
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
-    inputs = list()
-    try:
-        for i in range(len(results.pose_landmarks.landmark)):
-            inputs.append(results.pose_landmarks.landmark[i].x)
-            inputs.append(results.pose_landmarks.landmark[i].y)
-            inputs.append(results.pose_landmarks.landmark[i].z)
-    except:
-        pass
-    
-    output = modelo_cargado.predict(pd.DataFrame([inputs], columns=feature_names))
-    # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.putText(image, str(activities[output[0]]), (300, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    cv2.imshow('MediaPipe Pose', image)
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = pose.process(image)
+      inputs = list()
+      try:
+          for i in range(len(results.pose_landmarks.landmark)):
+              inputs.append(results.pose_landmarks.landmark[i].x)
+              inputs.append(results.pose_landmarks.landmark[i].y)
+              inputs.append(results.pose_landmarks.landmark[i].z)
+      except:
+          pass
+      
+      output = modelo_cargado.predict(pd.DataFrame([inputs], columns=feature_names))
+      # Draw the pose annotation on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      mp_drawing.draw_landmarks(
+          image,
+          results.pose_landmarks,
+          mp_pose.POSE_CONNECTIONS,
+          landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+      # Flip the image horizontally for a selfie-view display.
+      cv2.putText(image, str(activities[output[0]]), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+      cv2.imshow('MediaPipe Pose', image)
 
-    if cv2.waitKey(5) & 0xFF == ord('q'):
-        break
-cap.release()
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+  cap.release()
+  cv2.destroyAllWindows()
+
+else:
+  print("Invalid mode. Please use 'image' or 'video' as the second argument.")
+
+
